48921cf553e98d5a9e1f5f020daa113126460ed5
Vishal Munde <vmunde48@gmail.com>
2025-08-09 22:43:13 +0530

    Add OpenStreetMap attribution

--- public/js/map.js
+++ public/js/map.js
@@ -17,7 +17,7 @@ if (mapElement) {
 
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       maxZoom: 20,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
     const redIcon = L.icon({
